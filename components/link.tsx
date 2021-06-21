0df4b1e302e7fae227718d2fcc48e65795bbd630
NathanDM19 <creationmatee@gmail.com>
2021-06-21 20:49:16 +1000

    fixed crash in vercel

--- components/link.tsx
+++ components/link.tsx
@@ -1,13 +1,13 @@
 import { useRouter } from 'next/router'
 
-function Link({ children, href }) {
+function Link({ children, href }: any) {
   const router = useRouter()
   const style = {
     marginRight: 10,
     color: router.asPath === href ? 'red' : 'black',
   }
 
-  const handleClick = (e) => {
+  const handleClick = (e: any) => {
     e.preventDefault()
     router.push(href)
   }
